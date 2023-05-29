a933beb3b145d281b2d1f4d6d14ca62cf062d479
alper04 <128806070+alper04@users.noreply.github.com>
2023-05-29 02:36:30 +0200

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.adhdminiplayer {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-            
-                            
-    opens com.example.adhdminiplayer to javafx.fxml;
-    exports com.example.adhdminiplayer;
-}
